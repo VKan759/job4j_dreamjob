512144cc01a5a6f5d2f9602b793123a82798fe47
Vyacheslav Kan <vkan745@gmail.com>
2024-10-10 12:23:46 +0500

    4. Создание вакансии [#504849]

--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -3,9 +3,14 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.dreamjob.model.Vacancy;
 import ru.job4j.dreamjob.repository.MemoryVacancyRepository;
 import ru.job4j.dreamjob.repository.VacancyRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/vacancies")
@@ -21,5 +26,14 @@ public class VacancyController {
     @GetMapping("/create")
     public String getCreationPage() {
         return "vacancies/create";
+    }
+
+    @PostMapping("/create")
+    public String create(HttpServletRequest request) {
+        String title = request.getParameter("title");
+        String descripton = request.getParameter("descripton");
+        Vacancy vacancy = new Vacancy(0, title, descripton, LocalDateTime.now());
+        vacancyRepository.save(vacancy);
+        return "redirect:/vacancies";
     }
 }
