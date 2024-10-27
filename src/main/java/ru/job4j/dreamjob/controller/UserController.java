8dcd60c2ce981df807cc6db6b5256b1bd5d7e311
Vyacheslav Kan <vkan745@gmail.com>
2024-10-27 16:40:38 +0500

    1. Страница login.html [#504863]

--- src/main/java/ru/job4j/dreamjob/controller/UserController.java
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -2,10 +2,8 @@ package ru.job4j.dreamjob.controller;
 
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.dreamjob.model.User;
 import ru.job4j.dreamjob.service.UserService;
 
@@ -25,13 +23,27 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    @ResponseBody
     public String register(User user) {
         try {
             userService.save(user);
-            return "пользователь зарегистрирован";
+            return "users/login";
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "users/login";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@ModelAttribute User user, Model model) {
+        var userOptional = userService.findByEmailAndPassword(user.getEmail(), user.getPassword());
+        if (userOptional.isEmpty()) {
+            model.addAttribute("error", "Почта или пароль введены неверно");
+            return "users/login";
+        }
+        return "redirect:/vacancies";
     }
 }
